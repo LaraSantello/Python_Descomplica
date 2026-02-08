916f418abdeef5c303fe47d6025ef3901a8da9c7
LaraSantello <lara.santello@gmail.com>
2026-02-08 17:59:53 -0300

    second commit

---
+++ 2_Manipulacao_de_dados.py
@@ -0,0 +1,37 @@
+# Entrada de dados é tudo aquilo que utilizo como input ou o que o usuário digita
+# nome       = input("Digite seu nome: ")
+# nascimento = input("Qual seu ano de nascimento: ")
+# email      = input("Digite seu e-mail: ")
+# # print(nome)
+# # print(nascimento)
+# # print(email)
+
+# # Concatenando os resultados, deixando tudo em uam fraase só
+# print("Seu nome é: ", nome, ". Nascimento: ", nascimento, ". E-mail: ", email )
+
+####### TRATAMENTO DE INPUT/ OUTPUT
+# INPUT foi feito para que a gente insira texto (string), caracter, e não número, e se formos trabalhar com número, tem que colocar que estamos trabalhando com int
+# a = int(input("Digite um número: "))
+# print(a + 5)
+
+# b = float(input("Digite um número: "))
+# print(b + 5)
+
+####### OPERADORES BÁSICOS LÓGICOS
+# 1a Cat: Aritméticos
+## + - * /
+# 2a Cat: Relacionais
+## >(crevon) < >= <= == != ===(indenticidade)
+# 3a Cat: Lógicos
+## !   (NÃO)
+## &&  (E)
+## ||  (OU)
+
+# from cmath import sqrt
+# print(sqrt(4))
+# qualquer = 25
+# print(sqrt(qualquer))
+
+###### PACOTES
+from math import sqrt
+print(sqrt(4))
