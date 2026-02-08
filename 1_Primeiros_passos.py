5cd6e162378a9372e0669de042e4666676b44f31
LaraSantello <lara.santello@gmail.com>
2026-02-08 17:45:57 -0300

    first commit

---
+++ 1_Primeiros_passos.py
@@ -0,0 +1,21 @@
+a = 10
+b = "Descomplica"
+c = 40.5
+## Printar tudo de uma vez
+## A função type só deixa executar uma variável de cada vez
+print(type(a), type(b), type(c))
+## Printar cada um de uma vez
+print(type(a))
+print(type(b))
+print(type(c))
+
+## Constantes - Colocar sempre em LETRA MAIUSCULA, enquanto as variáveis simples, colocar sempre em minúsculo
+MAIORIDADE = 18
+MAIORIDADE = 21
+MAIORIDADE = 25
+
+
+## Três regrinhas padrões de variáveis simples para seguir:
+## 1ª regra: Não pode ter espaços nos nomes das variáveis
+## 2ª regra: Não pode começar com números
+## 3ª regra: Não pode ter caracteres especiais (ex.: !@#$*-/+~^)
